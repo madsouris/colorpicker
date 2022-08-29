24e43b151612301dfc4a24c541723e6deb06ce2c
madsouris <sourisservice@icloud.com>
2022-08-29 14:24:20 +0700

    added vite

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <ColorComponent />
+</template>
+
+<script lang="ts">
+    import ColorComponent from "./components/color.vue";
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        components: {
+            ColorComponent,
+        },
+    });
+</script>
+
+<style scoped></style>
